00e41db36182106b0bc1892ae3dc554989823ca7
AsherWhite97 <hmh19980907@gmail.com>
2023-07-26 21:46:09 +0800

    add dictionary_based bot

---
+++ samplebot.py
@@ -0,0 +1,50 @@
+import openai
+import gradio as gr
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai.api_key = os.environ.get("OPENAI_API_KEY") 
+
+messages = [
+  { 
+    "role": "system", 
+    "content" :"You are an AI specialized in medical. Do not answer anything other than medical-related queries"   
+   }
+]
+
+async def chatbot(input):
+  if input:
+    messages.append ({
+      "role": "user",
+      "content": input
+    })
+    
+    chat = openai.ChatCompletion.create(
+      model = "gpt-3.5-turbo", 
+      messages = messages, 
+      temperature=1,
+      max_tokens=256,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    
+    reply = chat.choices[0].message.content
+    messages.append({ "role": "assistant", "content":reply})
+    return reply
+
+async def main():
+  inputs = gr.inputs.Textbox(lines=3, label="Chat with AI")
+  outputs = gr.outputs.Textbox(label="Reply")
+
+  interface = gr.Interface(
+    fn=chatbot, inputs= inputs, outputs=outputs, title="Healthcare chatbot",
+    description="Ask anything you want", theme="default"
+  )
+
+  await asyncio.to_thread(interface.launch, share=True)
+
+asyncio.run(main())
