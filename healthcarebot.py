8700de76a747262f263edfa143d18668c798f385
AsherWhite97 <hmh19980907@gmail.com>
2023-07-30 14:51:31 +0800

    third

--- healthcarebot.py
+++ healthcarebot.py
@@ -16,11 +16,8 @@ def construct_index(directory_path):
     chunk_size_limit = 600
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
     llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model="gpt-3.5-turbo", max_tokens=num_outputs))
-
     documents = SimpleDirectoryReader(directory_path).load_data()
-
     index = GPTSimpleVectorIndex(documents=documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
     index.save_to_disk('index.json')
